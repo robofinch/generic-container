88f44515d4b01a8a4bf32d80d2c60e214b7bd369
robofinch <86991566+robofinch@users.noreply.github.com>
2025-07-13 15:34:30 -0500

    Add tests and more documentation for error types

--- crates/thread-checked-mutex/src/error.rs
+++ crates/thread-checked-mutex/src/error.rs
@@ -2,40 +2,103 @@ use std::{convert::Infallible, error::Error, sync::PoisonError};
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 
-pub trait HandlePoisonResult<R, S> {
+/// Extension trait for [`Result`] which adds the ability to more conveniently handle the poison
+/// errors returned by this crate's locks.
+///
+/// ## About Poison
+///
+/// When another thread panics while it holds a poisonable lock, the lock becomes poisoned (which
+/// may be manually cleared). Attempts to acquire a poisoned lock (or otherwise access, via the
+/// lock, the data protected by the lock) return poison errors, which act as speed bumps for
+/// accessing data whose logical invariants are potentially broken. See `std`'s [`PoisonError`] for
+/// more.
+///
+/// As a poison error is only returned when some thread has already panicked, it is common to
+/// unconditionally panic in the current thread as well when poison is encountered, or to simply
+/// ignore such a circumstance.
+///
+/// As a notable example, [`parking_lot`] does not provide poison errors at all, and does not care
+/// whether a different thread panicked while holding a [`parking_lot`] mutex. This is roughly
+/// equivalent to (but more performant than) using [`HandlePoisonResult::ignore_poison`]
+/// everywhere.
+///
+///
+/// [`parking_lot`]: https://docs.rs/parking_lot/
+pub trait HandlePoisonResult {
+    /// A variation of the `Self` result type which cannot possibly be a poison error.
+    type PoisonlessResult;
+
+    /// Silently converts any poison error into a successful result (see
+    /// [`PoisonError::into_inner`]), and otherwise returns the result unchanged.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[must_use]
-    fn ignore_poison(self) -> S;
-    fn panic_if_poison(self) -> S;
+    fn ignore_poison(self) -> Self::PoisonlessResult;
+
+    /// Panics if the result was caused by poison, and otherwise returns the result unchanged.
+    ///
+    /// # Panics
+    /// Panics if the result is an [`Err`] that was caused by poison.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
+    fn panic_if_poison(self) -> Self::PoisonlessResult;
 }
 
+/// Helper function to coerce an uninhabited poison error into `!`.
+#[inline]
+fn prove_unreachable(poison: &PoisonError<Infallible>) -> ! {
+    #[expect(clippy::uninhabited_references, reason = "this function is not reachable")]
+    match *poison.get_ref() {}
+}
+
+
+/// The result type returned by [`ThreadCheckedMutex::lock`].
+///
+/// [`ThreadCheckedMutex::lock`]: super::mutex::ThreadCheckedMutex::lock
 pub type LockResult<T> = Result<T, LockError<T>>;
+/// A variation of [`LockResult<T>`] which cannot possibly be a poison error.
 pub type PoisonlessLockResult<T> = Result<T, LockError<Infallible>>;
 
-impl<T> HandlePoisonResult<Self, PoisonlessLockResult<T>> for LockResult<T> {
+impl<T> HandlePoisonResult for LockResult<T> {
+    type PoisonlessResult = PoisonlessLockResult<T>;
+
+    /// Silently converts any poison error into a successful result (see
+    /// [`PoisonError::into_inner`]), and otherwise returns the result unchanged.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
-    fn ignore_poison(self) -> PoisonlessLockResult<T> {
+    fn ignore_poison(self) -> Self::PoisonlessResult {
         match self.map_err(LockError::ignore_poison) {
             Ok(t)                  => Ok(t),
             Err(poisonless_result) => poisonless_result,
         }
     }
 
+    /// Panics if the result was caused by poison, and otherwise returns the result unchanged.
+    ///
     /// # Panics
-    /// Panics if the result is an error that was caused by poison
-    /// (another thread panicking while they held a mutex related
-    /// to this result).
+    /// Panics if the result is an [`Err`] that was caused by poison.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
-    fn panic_if_poison(self) -> PoisonlessLockResult<T> {
+    fn panic_if_poison(self) -> Self::PoisonlessResult {
         self.map_err(LockError::panic_if_poison)
     }
 }
 
+/// An error that may be returned by [`ThreadCheckedMutex::lock`].
+///
+/// [`ThreadCheckedMutex::lock`]: super::mutex::ThreadCheckedMutex::lock
 pub enum LockError<T> {
     Poisoned(PoisonError<T>),
     LockedByCurrentThread,
 }
 
 impl<T> LockError<T> {
+    /// Silently converts any poison error into a successful result (see
+    /// [`PoisonError::into_inner`]), and otherwise returns the error unchanged in an [`Err`].
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
     pub fn ignore_poison(self) -> PoisonlessLockResult<T> {
         match self {
@@ -44,9 +107,12 @@ impl<T> LockError<T> {
         }
     }
 
+    /// Panics if the error was caused by poison, and otherwise returns the error unchanged.
+    ///
     /// # Panics
-    /// Panics if the error was caused by poison (another thread
-    /// panicking while they held a mutex related to this error).
+    /// Panics if the error was caused by poison.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
     #[must_use]
     pub fn panic_if_poison(self) -> LockError<Infallible> {
@@ -94,28 +160,57 @@ impl<T> Display for LockError<T> {
 
 impl<T> Error for LockError<T> {}
 
+impl PartialEq for LockError<Infallible> {
+    #[inline]
+    fn eq(&self, _other: &Self) -> bool {
+        // There's only one inhabited variant of `LockError<Infallible>`, so this returns true.
+        match self {
+            Self::LockedByCurrentThread => true,
+            Self::Poisoned(poison)      => prove_unreachable(poison),
+        }
+    }
+}
+
+impl Eq for LockError<Infallible> {}
+
+
+/// The result type returned by [`ThreadCheckedMutex::try_lock`].
+///
+/// [`ThreadCheckedMutex::try_lock`]: super::mutex::ThreadCheckedMutex::try_lock
 pub type TryLockResult<T> = Result<T, TryLockError<T>>;
+/// A variation of [`TryLockResult<T>`] which cannot possibly be a poison error.
 pub type PoisonlessTryLockResult<T> = Result<T, TryLockError<Infallible>>;
 
-impl<T> HandlePoisonResult<Self, PoisonlessTryLockResult<T>> for TryLockResult<T> {
+impl<T> HandlePoisonResult for TryLockResult<T> {
+    type PoisonlessResult = PoisonlessTryLockResult<T>;
+
+    /// Silently converts any poison error into a successful result (see
+    /// [`PoisonError::into_inner`]), and otherwise returns the result unchanged.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
-    fn ignore_poison(self) -> PoisonlessTryLockResult<T> {
+    fn ignore_poison(self) -> Self::PoisonlessResult {
         match self.map_err(TryLockError::ignore_poison) {
             Ok(t)                  => Ok(t),
             Err(poisonless_result) => poisonless_result,
         }
     }
 
+    /// Panics if the result was caused by poison, and otherwise returns the result unchanged.
+    ///
     /// # Panics
-    /// Panics if the result is an error that was caused by poison
-    /// (another thread panicking while they held a mutex related
-    /// to this result).
+    /// Panics if the result is an [`Err`] that was caused by poison.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
-    fn panic_if_poison(self) -> PoisonlessTryLockResult<T> {
+    fn panic_if_poison(self) -> Self::PoisonlessResult {
         self.map_err(TryLockError::panic_if_poison)
     }
 }
 
+/// An error that may be returned by [`ThreadCheckedMutex::try_lock`].
+///
+/// [`ThreadCheckedMutex::try_lock`]: super::mutex::ThreadCheckedMutex::try_lock
 pub enum TryLockError<T> {
     Poisoned(PoisonError<T>),
     LockedByCurrentThread,
@@ -123,6 +218,10 @@ pub enum TryLockError<T> {
 }
 
 impl<T> TryLockError<T> {
+    /// Silently converts any poison error into a successful result (see
+    /// [`PoisonError::into_inner`]), and otherwise returns the error unchanged in an [`Err`].
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
     pub fn ignore_poison(self) -> PoisonlessTryLockResult<T> {
         match self {
@@ -132,9 +231,12 @@ impl<T> TryLockError<T> {
         }
     }
 
+    /// Panics if the error was caused by poison, and otherwise returns the error unchanged.
+    ///
     /// # Panics
-    /// Panics if the error was caused by poison (another thread
-    /// panicking while they held a mutex related to this error).
+    /// Panics if the error was caused by poison.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
     #[must_use]
     pub fn panic_if_poison(self) -> TryLockError<Infallible> {
@@ -188,40 +290,92 @@ impl<T> Display for TryLockError<T> {
 
 impl<T> Error for TryLockError<T> {}
 
+impl PartialEq for TryLockError<Infallible> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            Self::LockedByCurrentThread => matches!(other, Self::LockedByCurrentThread),
+            Self::WouldBlock            => matches!(other, Self::WouldBlock),
+            Self::Poisoned(poison)      => prove_unreachable(poison),
+        }
+    }
+}
+
+impl Eq for TryLockError<Infallible> {}
+
+
+/// The result type returned by [`ThreadCheckedMutex::into_inner`] or
+/// [`ThreadCheckedMutex::get_mut`].
+///
+/// [`ThreadCheckedMutex::into_inner`]: super::mutex::ThreadCheckedMutex::into_inner
+/// [`ThreadCheckedMutex::get_mut`]: super::mutex::ThreadCheckedMutex::get_mut
 pub type AccessResult<T> = Result<T, AccessError<T>>;
+/// A variation of [`AccessResult<T>`] which cannot possibly be a poison error.
+///
+/// Note that every [`AccessError`] is caused by poison, so this result is always [`Ok`].
 pub type PoisonlessAccessResult<T> = Result<T, AccessError<Infallible>>;
 
-impl<T> HandlePoisonResult<Self, PoisonlessAccessResult<T>> for AccessResult<T> {
+impl<T> HandlePoisonResult for AccessResult<T> {
+    type PoisonlessResult = PoisonlessAccessResult<T>;
+
+    /// Silently converts any poison error into a successful result (see
+    /// [`PoisonError::into_inner`]), and otherwise returns the result unchanged.
+    ///
+    /// Since every [`AccessError`] is caused by poison, the returned result is always [`Ok`].
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
-    fn ignore_poison(self) -> PoisonlessAccessResult<T> {
+    fn ignore_poison(self) -> Self::PoisonlessResult {
         match self.map_err(AccessError::ignore_poison) {
             Ok(t)                  => Ok(t),
             Err(poisonless_result) => poisonless_result,
         }
     }
 
+    /// Panics if the error was caused by poison, and otherwise returns the error unchanged.
+    ///
+    /// Note that every [`AccessError`] is caused by poison, so this is similar to unwrapping the
+    /// result.
+    ///
     /// # Panics
-    /// Panics if the result is an error variant that was caused by
-    /// poison; note that `AccessError`s are all caused by poison.
+    /// Panics if the result is an [`Err`], which was necessarily caused by poison.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
-    fn panic_if_poison(self) -> PoisonlessAccessResult<T> {
+    fn panic_if_poison(self) -> Self::PoisonlessResult {
         self.map_err(|err| AccessError::panic_if_poison(err))
     }
 }
 
+/// An error that may be returned by [`ThreadCheckedMutex::into_inner`] or
+/// [`ThreadCheckedMutex::get_mut`].
+///
+/// [`ThreadCheckedMutex::into_inner`]: super::mutex::ThreadCheckedMutex::into_inner
+/// [`ThreadCheckedMutex::get_mut`]: super::mutex::ThreadCheckedMutex::get_mut
 pub struct AccessError<T> {
+    /// The only possible cause of an `AccessError` is a poisoned lock.
     pub poison: PoisonError<T>,
 }
 
 impl<T> AccessError<T> {
+    /// Silently converts any poison error into a successful result (see
+    /// [`PoisonError::into_inner`]).
+    ///
+    /// Since every [`AccessError`] is caused by poison, the returned result is always [`Ok`].
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
     pub fn ignore_poison(self) -> PoisonlessAccessResult<T> {
         Ok(self.poison.into_inner())
     }
 
+    /// Panics if the [`AccessError`] was caused by poison, which is always the case; this function
+    /// always panics.
+    ///
     /// # Panics
-    /// As an `AccessError` is always caused by a poison,
-    /// this function always panics.
+    /// Panics unconditionally, as the error is necessarily caused by poison.
+    ///
+    /// [Read more about poison.](HandlePoisonResult#about-poison)
     #[inline]
     pub fn panic_if_poison(self) -> ! {
         #![expect(
@@ -254,3 +408,137 @@ impl<T> Display for AccessError<T> {
 }
 
 impl<T> Error for AccessError<T> {}
+
+impl PartialEq for AccessError<Infallible> {
+    #[inline]
+    fn eq(&self, _other: &Self) -> bool {
+        prove_unreachable(&self.poison)
+    }
+}
+
+impl Eq for AccessError<Infallible> {}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn lock_ignore_poison() {
+        // Ok
+        let res: LockResult<()> = Ok(());
+        assert!(matches!(res.ignore_poison(), Ok(())));
+
+        // Err but not poison
+        let res: LockResult<()> = Err(LockError::LockedByCurrentThread);
+        assert!(matches!(res.ignore_poison(), Err(LockError::LockedByCurrentThread)));
+
+        // Poison
+        let res: LockResult<()> = Err(PoisonError::new(()).into());
+        assert!(matches!(res.ignore_poison(), Ok(())));
+    }
+
+    #[test]
+    fn lock_panic_if_poison() {
+        // Ok
+        let res: LockResult<()> = Ok(());
+        assert!(matches!(res.panic_if_poison(), Ok(())));
+
+        // Err but not poison
+        let res: LockResult<()> = Err(LockError::LockedByCurrentThread);
+        assert!(matches!(res.panic_if_poison(), Err(LockError::LockedByCurrentThread)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn panicking_lock_panic_if_poison() {
+        // Poison
+        let res: LockResult<()> = Err(PoisonError::new(()).into());
+        let _ = res.panic_if_poison();
+    }
+
+    #[test]
+    fn try_lock_ignore_poison() {
+        // Ok
+        let res: TryLockResult<()> = Ok(());
+        assert!(matches!(res.ignore_poison(), Ok(())));
+
+        // Err but not poison
+        let res: TryLockResult<()> = Err(TryLockError::LockedByCurrentThread);
+        assert!(matches!(res.ignore_poison(), Err(TryLockError::LockedByCurrentThread)));
+
+        // Poison
+        let res: TryLockResult<()> = Err(PoisonError::new(()).into());
+        assert!(matches!(res.ignore_poison(), Ok(())));
+    }
+
+    #[test]
+    fn try_lock_panic_if_poison() {
+        // Ok
+        let res: TryLockResult<()> = Ok(());
+        assert!(matches!(res.panic_if_poison(), Ok(())));
+
+        // Err but not poison
+        let res: TryLockResult<()> = Err(TryLockError::LockedByCurrentThread);
+        assert!(matches!(res.panic_if_poison(), Err(TryLockError::LockedByCurrentThread)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn panicking_try_lock_panic_if_poison() {
+        // Poison
+        let res: TryLockResult<()> = Err(PoisonError::new(()).into());
+        let _ = res.panic_if_poison();
+    }
+
+    #[test]
+    fn access_ignore_poison() {
+        // Ok
+        let res: AccessResult<()> = Ok(());
+        assert!(matches!(res.ignore_poison(), Ok(())));
+
+        // Err but not poison.. is impossible.
+
+        // Poison
+        let res: AccessResult<()> = Err(PoisonError::new(()).into());
+        assert!(matches!(res.ignore_poison(), Ok(())));
+    }
+
+    #[test]
+    fn access_panic_if_poison() {
+        // Ok
+        let res: AccessResult<()> = Ok(());
+       assert!(matches!(res.panic_if_poison(), Ok(())));
+
+        // Err but not poison.. is impossible.
+    }
+
+    #[test]
+    #[should_panic]
+    fn panicking_access_panic_if_poison() {
+        // Poison
+        let res: AccessResult<()> = Err(PoisonError::new(()).into());
+        let _ = res.panic_if_poison();
+    }
+
+    fn test_eq_impl<E: Eq, const N: usize>(errors: [E; N]) {
+        for (i, error) in errors.iter().enumerate() {
+            for (j, other) in errors.iter().enumerate() {
+                assert_eq!(i == j, error == other);
+            }
+        }
+    }
+
+    #[test]
+    fn eq_impls() {
+        // The `::<Infallible>`s are not strictly necessary, but make it more clear.
+        test_eq_impl([
+            LockError::<Infallible>::LockedByCurrentThread,
+        ]);
+        test_eq_impl([
+            TryLockError::<Infallible>::LockedByCurrentThread,
+            TryLockError::<Infallible>::WouldBlock,
+        ]);
+        // `AccessError<Infallible>` is uninhabited.
+    }
+}
